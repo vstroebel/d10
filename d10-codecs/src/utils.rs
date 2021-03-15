5d5edf3a1c0ede70fdcb4c45e5c4de3f54113d31
Volker Ströbel <volkerstroebel@mysurdity.de>
2021-03-15 10:18:28 +0100

    Fix converting color value to u8

--- d10-codecs/src/utils.rs
+++ d10-codecs/src/utils.rs
@@ -3,7 +3,7 @@ use d10_core::color::{Color, RGB};
 
 /// Convert color channel value between 0.0 and 1.0 into an u8
 pub(crate) fn as_u8(value: f32) -> u8 {
-    value.clamp(0.0, 1.0) as u8
+    (value * 255.0).clamp(0.0, 255.0) as u8
 }
 
 pub(crate) fn to_l8_vec(buffer: &PixelBuffer<RGB>) -> Vec<u8> {
