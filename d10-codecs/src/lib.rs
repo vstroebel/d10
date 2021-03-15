0c13991c71ffb3c3de89ffdbe82f824740c7174f
Volker Ströbel <volkerstroebel@mysurdity.de>
2021-03-15 09:50:42 +0100

    Add PNG format support to save
    Only 8 bit color formats are support and some image crate enums (CompressionType and FilterType) a bleeding into the public api

--- d10-codecs/src/lib.rs
+++ d10-codecs/src/lib.rs
@@ -1,6 +1,6 @@
 mod utils;
 
-use utils::as_u8;
+use utils::*;
 
 use d10_core::pixelbuffer::PixelBuffer;
 use d10_core::color::{Color, RGB, SRGB};
@@ -9,16 +9,32 @@ use d10_core::errors::{D10Result, D10Error};
 use image::{DynamicImage, GenericImageView, ImageError, ImageBuffer, Rgba, ColorType};
 use image::io::Reader;
 use image::codecs::jpeg::JpegEncoder;
+use image::codecs::png::PngEncoder;
 
 use std::path::Path;
 use std::io::{Cursor, Read, Seek, BufRead};
 use std::fs::File;
 
+//TODO: Wrap types to not export image crate internals
+pub use image::codecs::png::CompressionType as PNGCompressionType;
+pub use image::codecs::png::FilterType as PNGFilterType;
+
+pub enum PNGColorType {
+    L8,
+    LA8,
+    RGB8,
+    RGBA8,
+}
 
 pub enum Format {
     Auto,
     JPEG {
         quality: u8
+    },
+    PNG {
+        color_type: PNGColorType,
+        compression: PNGCompressionType,
+        filter: PNGFilterType,
     },
 }
 
@@ -121,21 +137,13 @@ fn decode<T>(reader: Reader<T>) -> D10Result<DecodedImage> where T: Read + Seek 
 pub fn save_to_file<P>(path: P, buffer: &PixelBuffer<RGB>, format: Format) -> D10Result<()> where P: AsRef<Path> {
     match format {
         Format::JPEG { quality } => save_to_file_jpeg(path, buffer, quality),
+        Format::PNG { color_type, compression, filter } => save_to_file_png(path, buffer, color_type, compression, filter),
         Format::Auto => save_to_file_auto(path, buffer)
     }
 }
 
 fn save_to_file_auto<P>(path: P, buffer: &PixelBuffer<RGB>) -> D10Result<()> where P: AsRef<Path> {
-    let mut out: Vec<u8> = Vec::with_capacity(buffer.width() as usize * buffer.height() as usize * 4);
-
-    for color in buffer.data().iter() {
-        let color = color.to_srgb();
-
-        out.push(as_u8(color.red()));
-        out.push(as_u8(color.green()));
-        out.push(as_u8(color.blue()));
-        out.push(as_u8(color.alpha()));
-    }
+    let out = to_rgba32_vec(buffer);
 
     let out: ImageBuffer<Rgba<u8>, Vec<u8>> = ImageBuffer::from_vec(buffer.width(), buffer.height(), out)
         .ok_or_else(|| D10Error::OpenError("Unable to create buffer".to_owned()))?;
@@ -150,19 +158,29 @@ fn save_to_file_auto<P>(path: P, buffer: &PixelBuffer<RGB>) -> D10Result<()> whe
 }
 
 fn save_to_file_jpeg<P>(path: P, buffer: &PixelBuffer<RGB>, quality: u8) -> D10Result<()> where P: AsRef<Path> {
-    let mut out: Vec<u8> = Vec::with_capacity(buffer.width() as usize * buffer.height() as usize * 3);
-
-    for color in buffer.data().iter() {
-        let color = color.to_srgb();
-
-        out.push(as_u8(color.red()));
-        out.push(as_u8(color.green()));
-        out.push(as_u8(color.blue()));
-    }
+    let out = to_rgb24_vec(buffer);
 
     let mut result = File::open(path)?;
 
     if let Err(err) = JpegEncoder::new_with_quality(&mut result, quality).encode(&out, buffer.width(), buffer.height(), ColorType::Rgb8) {
+        Err(D10Error::SaveError(format!("Save error: {:?}", err)))
+    } else {
+        Ok(())
+    }
+}
+
+fn save_to_file_png<P>(path: P, buffer: &PixelBuffer<RGB>, color_type: PNGColorType, compression: PNGCompressionType, filter: PNGFilterType) -> D10Result<()> where P: AsRef<Path> {
+    let (out, color_type) = match color_type {
+        PNGColorType::L8 => (to_l8_vec(buffer), ColorType::L8),
+        PNGColorType::LA8 => (to_la8_vec(buffer), ColorType::La8),
+        PNGColorType::RGB8 => (to_rgb24_vec(buffer), ColorType::Rgb8),
+        PNGColorType::RGBA8 => (to_rgba32_vec(buffer), ColorType::Rgba8)
+    };
+
+    let mut result = File::open(path)?;
+
+    if let Err(err) = PngEncoder::new_with_quality(&mut result, compression, filter)
+        .encode(&out, buffer.width(), buffer.height(), color_type) {
         Err(D10Error::SaveError(format!("Save error: {:?}", err)))
     } else {
         Ok(())
--- d10-codecs/src/utils.rs
+++ d10-codecs/src/utils.rs
@@ -1,4 +1,60 @@
+use d10_core::pixelbuffer::PixelBuffer;
+use d10_core::color::{Color, RGB};
+
 /// Convert color channel value between 0.0 and 1.0 into an u8
 pub(crate) fn as_u8(value: f32) -> u8 {
     value.clamp(0.0, 1.0) as u8
 }
+
+pub(crate) fn to_l8_vec(buffer: &PixelBuffer<RGB>) -> Vec<u8> {
+    let mut out: Vec<u8> = Vec::with_capacity(buffer.width() as usize * buffer.height() as usize);
+
+    for color in buffer.data().iter() {
+        let color = color.to_gray().to_srgb();
+        out.push(as_u8(color.red()));
+    }
+
+    out
+}
+
+pub(crate) fn to_la8_vec(buffer: &PixelBuffer<RGB>) -> Vec<u8> {
+    let mut out: Vec<u8> = Vec::with_capacity(buffer.width() as usize * buffer.height() as usize * 2);
+
+    for color in buffer.data().iter() {
+        let color = color.to_gray().to_srgb();
+        out.push(as_u8(color.red()));
+        out.push(as_u8(color.alpha()));
+    }
+
+    out
+}
+
+pub(crate) fn to_rgb24_vec(buffer: &PixelBuffer<RGB>) -> Vec<u8> {
+    let mut out: Vec<u8> = Vec::with_capacity(buffer.width() as usize * buffer.height() as usize * 3);
+
+    for color in buffer.data().iter() {
+        let color = color.to_srgb();
+
+        out.push(as_u8(color.red()));
+        out.push(as_u8(color.green()));
+        out.push(as_u8(color.blue()));
+    }
+
+    out
+}
+
+pub(crate) fn to_rgba32_vec(buffer: &PixelBuffer<RGB>) -> Vec<u8> {
+    let mut out: Vec<u8> = Vec::with_capacity(buffer.width() as usize * buffer.height() as usize * 4);
+
+    for color in buffer.data().iter() {
+        let color = color.to_srgb();
+
+        out.push(as_u8(color.red()));
+        out.push(as_u8(color.green()));
+        out.push(as_u8(color.blue()));
+        out.push(as_u8(color.alpha()));
+    }
+
+    out
+}
+
