460fd0d6962574d4bea62ed2e08dd9206bb2c97a
Volker Ströbel <volkerstroebel@mysurdity.de>
2021-04-13 10:24:46 +0200

    Reduce errornous black dots in drawing

--- d10-ops/src/drawing.rs
+++ d10-ops/src/drawing.rs
@@ -86,6 +86,8 @@ pub fn drawing(buffer: &PixelBuffer<Rgb>, radius: u32, mode: DrawingMode) -> Pix
         Rgb::new(diff, diff, diff)
     });
 
+    let drawing = despeckle(&drawing, 2);
+
     let drawing = unsharp(&drawing, 2, 1.0, None);
 
     match mode {
@@ -139,4 +141,43 @@ fn merge_color_reduced(drawing: PixelBuffer<Rgb>, orig: &PixelBuffer<Rgb>) -> Pi
                 .with_gamma(1.1)
                 .with_vibrance(0.3)
         )
+}
+
+fn despeckle(img: &PixelBuffer<Rgb>, amount: u32) -> PixelBuffer<Rgb> {
+    img.map_colors_enumerated(|x, y, c| {
+        if c.to_gray_with_intensity(Intensity::Average).red() < 0.05 {
+            let k = img.get_kernel::<3>(x as i32, y as i32);
+
+            let mut count = 0;
+
+            for y in 0..3 {
+                for x in 0..3 {
+                    if k[y][x].to_gray_with_intensity(Intensity::Average).red() < 0.05 {
+                        count += 1;
+                    }
+                }
+            }
+
+            if count <= amount {
+                let mut data = [0f32; 3];
+
+                for y in 0..3 {
+                    for x in 0..3 {
+                        let kc = k[y][x];
+                        if kc.to_gray_with_intensity(Intensity::Average).red() >= 0.05 {
+                            data[0] += kc.data[0];
+                            data[1] += kc.data[1];
+                            data[2] += kc.data[2];
+                        }
+                    }
+                }
+
+                let sum = (9 - count) as f32;
+
+                return Rgb::new_with_alpha(data[0] / sum, data[1] / sum, data[2] / sum, c.alpha());
+            }
+        }
+
+        *c
+    })
 }
